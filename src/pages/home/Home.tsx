ab8c520cc4798d6f4d0f58a06137f72a297ea871
ronyislam502 <ronyislam502@gmail.com>
2024-09-30 19:29:00 +0600

    home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,15 @@
 import Banner from "./components/Banner";
+import Review from "./components/review";
+import Services from "./components/services";
+
 import "./style.css";
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <Services />
+      <Review />
     </div>
   );
 };
